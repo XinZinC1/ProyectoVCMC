12c9ea8ee77d99a1f63547c8a1b97a0c855eb875
hector-ar-24 <hector.aguirre@usil.pe>
2024-06-09 15:40:28 -0500

    Generar datos de las imagenes
    uso de librería pickle para guardar los datos transformados de las imágenes

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import cv2
 import os
 import matplotlib.pyplot as plt
-from PIL import Image, ImageEnhance, ImageOps
+from PIL import Image, ImageEnhance
 import numpy as np
-DATADIR=r"ClasificacionHojas"
+from tqdm import tqdm
+import random as rn
+import pickle 
+DATADIR=r"Clasificacion_Hojas"
 CATEGORIAS=["ENFERMA","SANA"]
+tamaño_img=2040
 def Generar_datos():
+    data=[]
     for categoria in CATEGORIAS:
         carpeta= os.path.join(DATADIR,categoria)
+        valor=CATEGORIAS.index(categoria)
         # Parámetros de edición
         brillo = 1.75  # Ajusta el brillo (1.0 = sin cambios)
         contraste = 1.05  # Ajusta el contraste (1.0 = sin cambios)
@@ -15,9 +21,13 @@ def Generar_datos():
         sombras = 1.05 # Ajusta las sombras (1.0 = sin cambios)
         nitidez = 1.5  # Ajusta la nitidez (1.0 = sin cambios)
 
-        for filename in os.listdir(carpeta):
-            if filename.endswith('.jpg') or filename.endswith('.png'):
-                ruta = os.path.join(carpeta, filename)
+        #para barra de carga
+        list=os.listdir(carpeta)
+
+        for i in tqdm(range(len(list)), desc=categoria):
+            nombre_imagen= list[i]
+            if nombre_imagen.endswith('.jpg') or nombre_imagen.endswith('.png'):
+                ruta = os.path.join(carpeta, nombre_imagen)
                 imagen = Image.open(ruta)
 
                 # Ajusta el brillo
@@ -36,15 +46,36 @@ def Generar_datos():
                 ajuste_nitidez = ImageEnhance.Sharpness(imagen)
                 imagen = ajuste_nitidez.enhance(nitidez)
 
-                # Convertir la imagen a formato OpenCV
-                imagen_cv = cv2.cvtColor(np.array(imagen), cv2.COLOR_RGB2BGR)
+                # Convertir la imagen a formato OpenCV y bn
+                imagen_cv = cv2.cvtColor(np.array(imagen), cv2.COLOR_BGR2GRAY)
+                #homogenizar tamaño de imagenes
+                imagen_cv=cv2.resize(imagen_cv,(tamaño_img,tamaño_img))
+
                 # Aplicar filtro de Sobel
                 bordes_verticales = cv2.Sobel(imagen_cv, cv2.CV_8U, 1, 0, ksize=3)
                 bordes_horizontales = cv2.Sobel(imagen_cv, cv2.CV_8U, 0, 1, ksize=3)
                 gradiente = cv2.addWeighted(bordes_verticales, 0.5, bordes_horizontales, 0.5, 0)
+                #añadir a lista data
+                data.append([gradiente,valor])
 
                 # Mostrar la imagen con el filtro de Sobel
-                plt.imshow(gradiente, cmap='gray')
-                plt.show()
+                #plt.imshow(gradiente, cmap='gray')
+                #plt.show()
+    rn.shuffle(data)
+    x=[]
+    y=[]
+    lista=data
+    for i in tqdm(range(len(lista)), desc="procesando"):
+        par=data[i]
+        x.append(par[0]) 
+        y.append(par[1])
+    x=np.array(x).reshape(-1,2040,2040,1)
 
+    pickle_out=open("x.pickle","wb")
+    pickle.dump(x,pickle_out)
+    pickle_out.close()
+
+    pickle_out=open("y.pickle","wb")
+    pickle.dump(y,pickle_out)
+    pickle_out.close()
 Generar_datos()
