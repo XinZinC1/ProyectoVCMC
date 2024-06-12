260e97360a493ea4f4b7530ef5617b9551f2da7e
hector-ar-24 <hector.aguirre@usil.pe>
2024-06-11 20:46:55 -0500

    actualizado

--- entrenamiento.py
+++ entrenamiento.py
@@ -29,22 +29,22 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 # Crear la red neuronal
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(tamaño_img, tamaño_img, 1)))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
-model.add(Flatten())
-model.add(Dense(2, activation='softmax'))
+modelo = Sequential()
+modelo.add(Conv2D(32, (3, 3), activation='relu', input_shape=(tamaño_img, tamaño_img, 1)))
+modelo.add(MaxPooling2D((2, 2)))
+modelo.add(Conv2D(64, (3, 3), activation='relu'))
+modelo.add(MaxPooling2D((2, 2)))
+modelo.add(Conv2D(128, (3, 3), activation='relu'))
+modelo.add(MaxPooling2D((2, 2)))
+modelo.add(Flatten())
+modelo.add(Dense(2, activation='softmax'))
 
 # Compilar la red neuronal
-model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+modelo.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Configurar los callbacks
 early_stopping = EarlyStopping(monitor='val_loss', patience=5, min_delta=0.001)
 model_checkpoint = ModelCheckpoint('best_model.keras', monitor='val_loss', save_best_only=True, mode='min')
 
 # Entrenar la red neuronal
-model.fit(x_train, y_train, epochs=100, batch_size=32, validation_data=(x_test, y_test), callbacks=[early_stopping, model_checkpoint])
+modelo.fit(x_train, y_train, epochs=5, batch_size=32, validation_data=(x_test, y_test), callbacks=[early_stopping, model_checkpoint])
