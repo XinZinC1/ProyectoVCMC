8887f9e679f54038980ece3fc4002384c631c572
hector-ar-24 <hector.aguirre@usil.pe>
2024-06-18 19:16:26 -0500

    actual
    iteracion de neuronas, guardado de modelos en carpeta

--- entrenamiento.py
+++ entrenamiento.py
@@ -4,8 +4,7 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras.utils import to_categorical
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
-
+from keras.callbacks import EarlyStopping, ModelCheckpoint
 tamaño_img=720
 
 # Cargar los datos
@@ -29,27 +28,25 @@ y = to_categorical(y)
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-neuronas=[32]
-for neurona in neuronas:
-    name= "RedConv_n{}_",format(neurona)
-    tensorboard= TensorBoard(log_dir='logs/{}'.format(name))
-# Crear la red neuronal
+# Iterar sobre el número de neuronas
+for num_neurons in [16, 32, 64, 128]:
+    # Crear la red neuronal
     modelo = Sequential()
-    modelo.add(Conv2D(neurona, (3, 3), activation='relu', input_shape=(tamaño_img, tamaño_img, 1)))
+    modelo.add(Conv2D(num_neurons, (3, 3), activation='relu', input_shape=(tamaño_img, tamaño_img, 1)))
     modelo.add(MaxPooling2D((2, 2)))
-    modelo.add(Conv2D(neurona, (3, 3), activation='relu'))
+    modelo.add(Conv2D(2 * num_neurons, (3, 3), activation='relu'))
     modelo.add(MaxPooling2D((2, 2)))
-    modelo.add(Conv2D(neurona, (3, 3), activation='relu'))
+    modelo.add(Conv2D(4 * num_neurons, (3, 3), activation='relu'))
     modelo.add(MaxPooling2D((2, 2)))
     modelo.add(Flatten())
     modelo.add(Dense(2, activation='softmax'))
-# Compilar la red neuronal
+
+    # Compilar la red neuronal
     modelo.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Configurar los callbacks
-#early_stopping = EarlyStopping(monitor='val_loss', patience=5, min_delta=0.001)
-#model_checkpoint = ModelCheckpoint('best_model.keras', monitor='val_loss', save_best_only=True, mode='min')
+    # Configurar los callbacks
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, min_delta=0.001)
+    model_checkpoint = ModelCheckpoint('modelos/cnn_model_neu{}.keras'.format(num_neurons), monitor='val_loss', save_best_only=True, mode='min')
 
-# Entrenar la red neuronal
-    modelo.fit(x_train, y_train, epochs=2, batch_size=3, validation_data=(x_test, y_test), callbacks=[tensorboard])
-    modelo.save('models/{}',format(name))
+    # Entrenar la red neuronal
+    modelo.fit(x_train, y_train, epochs=1, batch_size=32, validation_data=(x_test, y_test), callbacks=[early_stopping, model_checkpoint])
